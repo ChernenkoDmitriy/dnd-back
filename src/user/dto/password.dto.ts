ed968b7efefcb562c15d020a2d10aa394f882742
Sluip430 <s12122000@gmail.com>
2022-06-20 01:48:40 +0300

    clear master branch

--- src/user/dto/password.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
-
-export class PasswordDto {
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(4)
-  @MaxLength(20)
-  password: string;
-}
